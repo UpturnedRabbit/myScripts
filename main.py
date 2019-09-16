43c9cc28003b1b87ef58412ccc1dc27cb42b3b22
eclaxton <eclaxton@deltacontrols.com>
2019-09-16 09:59:47 -0700

    Learning OOP with corey shaefer, video 4

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hi from work")
+class Employee:
+
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+
+emp_1 = Employee('Corey', 'Shaefer', 50000)
+emp_2 = Employee('Test', 'User', 60000)
