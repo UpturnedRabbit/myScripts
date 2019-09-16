c9b74f411b3cc39dcafbefdf88c9195a6955ae3e
eclaxton <eclaxton@deltacontrols.com>
2019-09-16 11:29:33 -0700

    decorators, vid 6, very cool

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
 class Employee:
 
-    raise_amt = 1.04
-
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.email = first + '.' + last + '@email.com'
-        self.pay = pay
 
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amt)
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
 
-    def __repr__(self):
-        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
 
-    def __str__(self):
-        return '{} - {}'.format(self.fullname(), self.email)
-
-    def __add__(self, other):
-        return self.pay + other.pay
-
-    def __len__(self):
-        return len(self.fullname())
-
-
-emp_1 = Employee('Corey', 'Shaefer', 50000)
-emp_2 = Employee('Test', 'User', 60000)
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
 
 
-print(len(emp_1))
+emp_1 = Employee('Corey', 'Shaefer')
 
+print(emp_1.first)
 
-# print(emp_1 + emp_2)
-# print(repr(emp_1))
-# print(str(emp_1))
+del emp_1.fullname
+emp_1.fullname = "Eric Claxton"
 
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
