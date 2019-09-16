f2d94a47b263c1adc7f65187f2305e911a9a5de6
eclaxton <eclaxton@deltacontrols.com>
2019-09-16 11:15:20 -0700

    end of tutorial 5

--- main.py
+++ main.py
@@ -14,6 +14,27 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
 
+    def __repr__(self):
+        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
+
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.fullname())
+
 
 emp_1 = Employee('Corey', 'Shaefer', 50000)
 emp_2 = Employee('Test', 'User', 60000)
+
+
+print(len(emp_1))
+
+
+# print(emp_1 + emp_2)
+# print(repr(emp_1))
+# print(str(emp_1))
+
